d68bba8b98d40ab74b068d48a7b13620b7212611
Neutrino666 <amupckuu@yandex.ru>
2022-07-18 01:51:52 +0300

    added assert in test_items.py

--- test_items.py
+++ test_items.py
@@ -5,8 +5,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 BTN_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayed(browser):
     browser.get(link)
-    browser.find_element(*BTN_ADD_TO_BASKET)
+    btn = browser.find_element(*BTN_ADD_TO_BASKET)
+    assert btn.is_displayed(), "Кнопка не видна"
     # sleep исключительно для удобства проверки работоспособности теста
     time.sleep(3)
