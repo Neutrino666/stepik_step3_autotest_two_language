c0eb497893f5eb9d1205d5f4a78f1c3fac40d02e
Neutrino666 <amupckuu@yandex.ru>
2022-07-18 01:19:11 +0300

    added sleep

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 BTN_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
@@ -7,3 +8,5 @@ BTN_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(*BTN_ADD_TO_BASKET)
+    # sleep исключительно для удобства проверки работоспособности теста
+    time.sleep(3)
